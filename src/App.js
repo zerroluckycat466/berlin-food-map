95c3645866f4bf2573c0cc6ccaa0017f0599a84e
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-01 23:54:50 +0800

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen bei der Berlin Food Map!</h1>
+      {/* Hier wird die interaktive Karte integriert */}
+    </div>
+  );
+}
+
+export default App;
