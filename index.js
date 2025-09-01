99e683cf2c38c57f0ab08a4c20fb7d416e46d65b
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-01 23:55:27 +0800

    Update MongoDB connection string to use environment variable for better configuration.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 // MongoDB-Verbindung
-mongoose.connect('mongodb://localhost:27017/berlin_food_map', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/berlin_food_map', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB verbunden'))
   .catch(err => console.log(err));
 
